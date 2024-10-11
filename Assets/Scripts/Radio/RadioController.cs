35a5d5ccaee1d11b1d96074f30fc60c4b12944dd
pavlo-kokhan <pavlokokhan@gmail.com>
2024-10-11 20:20:44 +0300

    LayerMask for clickable objects, can	 open multiple interaction pannels with same mask

--- Assets/Scripts/Radio/RadioController.cs
+++ Assets/Scripts/Radio/RadioController.cs
@@ -1,52 +1,9 @@
-using UnityEngine;
+using Core;
 
 namespace Radio
 {
-    public class RadioController : MonoBehaviour
+    public class RadioController : ClickableObjectController
     {
-        [SerializeField] private GameObject radioUI;
         
-        private SpriteRenderer _spriteRenderer;
-        private PolygonCollider2D _collider;
-
-        private void Awake()
-        {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            _collider = GetComponent<PolygonCollider2D>();
-        }
-
-        private void Update()
-        {
-            if (IsMouseOver())
-            {
-                _spriteRenderer.color = new Color(1f, 0.9f, 0.9f); // todo
-                
-                if (Input.GetMouseButtonDown(0))
-                {
-                    radioUI.SetActive(true);
-                }
-            }
-            else
-            {
-                _spriteRenderer.color = Color.white;
-            }
-            
-            if (Input.GetKeyUp(KeyCode.Escape))
-            {
-                radioUI.SetActive(false);
-            }
-        }
-        
-        private bool IsMouseOver()
-        {
-            if (Camera.main != null)
-            {
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
-                return _collider.OverlapPoint(mousePosition);
-            }
-
-            return false; // todo
-        }
     }
 }
